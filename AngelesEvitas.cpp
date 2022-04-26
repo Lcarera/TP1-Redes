40eea2093124dd9c78641fb5ec0b83a90c230970
lcarera <lcarera04@gmail.com>
2022-04-25 21:16:55 -0300

    segunda final codigo

--- AngelesEvitas.cpp
+++ AngelesEvitas.cpp
@@ -9,87 +9,101 @@
 #include <atomic>
 #include <tuple>
 #include <cassert>
+#include <fstream>
 
 using namespace std;
 
 float generarNumeroRandom();
-tuple<string,double, int, int> crearHijos(float probabilidadNacimiento, float probabilidadBerserker, int intentosProcreacion);
 
-int main()
+tuple<string,double, int, int, string> crearHijos(float probabilidadNacimiento, float probabilidadBerserker, int intentosProcreacion);
+
+void* share_mem(int size);
+
+void verificarResultado(float eps, int maximoDias, atomic<float>* evitas, atomic<float>* angeles);
+
+void agregarHijo(atomic<float>* lista, int dia, float poder, int maximoDias);
+
+void experimento(float probabilidadNacimiento, float probabilidadBerserker, int intentosProcreacion, atomic<float>* lista, int maximoDias);
+
+int main(int argc, char* argv[])
 {
-    int dias_max;
-    int dia;
-    int m; //Intentos de reproduccion, cantidad de hijos que van a intentar tener
-    int n; //Intentos de reproduccion, cantidad de hijos que van a intentar tener
-    float p1; //Probabilidad de nacimiento angeles
-    float p2; //Probabilidad de nacimiento evitas
-    float p3;
-    cout << "Ingrese dias: ";
-    cin >> dias_max;
-    cout << "Ingrese intentos de reproduccion deseados de angeles: ";
-    cin >> m;
-    cout << "Ingrese intentos de reproduccion deseados de evitas: ";
-    cin >> n;
-    cout << "Ingrese la probabilidad de nacimiendo de un angel: ";
-    cin >> p1;
-    cout << "Ingrese la probabilidad de nacimiendo de un evita: ";
-    cin >> p2;
-    cout << "Ingrese la probabilidad de nacimiendo de un evita berserker: ";
-    cin >> p3;
+    // Declaracion de valores inciales
+    int maximoDias = atoi(argv[1]); 
+	int M = atoi(argv[2]); 
+	int N = atoi(argv[3]); 
+	float ps[3];
+	ps[0] = stof(argv[4]);
+	ps[1] = stof(argv[5]);
+	ps[2] = stof(argv[6]);
+	float eps = stof(argv[7]);
 
+    int size = maximoDias;
+	atomic<float> *evitas = (atomic<float> *) share_mem(size * sizeof(atomic<int>));
+	atomic<float> *angeles = (atomic<float> *) share_mem(size * sizeof(atomic<int>));
+	
+	for(int i = 0;i < 2*size; i++)evitas[i] = 0;
+	for(int i = 0;i < 2*size; i++)angeles[i] = 0;
+
+	evitas[0] = 1;
+	angeles[0] = 1;	
+
+    //Comienzo de experimento
     int mainPid = getpid();
     int childPid = 0;
     cout << "Main pid es: " << mainPid << " Terminal pid:" << getppid()  << endl;
     
     int adanPid = fork();
     cout << "Adan pid: " <<  adanPid << endl;
+    float probabilidadNacimiento;
+    float probabilidadBerserker;
+    int intentosProcreacion;
+    atomic<float> *lista;
     if (adanPid == 0)
     {
-        for (int i = 1; i < dias_max; i++)
-        {    
-            auto t = crearHijos(p1,1.1,m);
-
-            auto [a, b, c, d] = t;
-            cout << "Tipo: " << a << " Poder: " << b << " Pid: "<< c <<" PPID:"<< d<<endl;
-        }
+        probabilidadNacimiento = ps[0];
+        probabilidadBerserker = 1.1;
+        intentosProcreacion = M; 
+        lista = angeles;
     }
     else
     {
-        int evaPid = fork();
-        cout << "Eva pid: " <<  evaPid << endl;
-        if (evaPid == 0)
+        int liliPid = fork();
+        cout << "Lili pid: " <<  liliPid << endl;
+        if (liliPid == 0)
         {
-            for (int i = 1; i < dias_max; i++)
-            {    
-                auto t = crearHijos(p2,p3,n);
-
-                auto [a, b, c, d] = t;
-                cout << "Tipo: " << a << " Poder: " << b << " Pid: "<< c <<" PPID:"<< d<<endl;
+            probabilidadNacimiento = ps[1];
+            probabilidadBerserker = ps[2];
+            intentosProcreacion = N;
+            lista = evitas;
+        }
+        else
+        {
+            wait(NULL);
+            wait(NULL);
+            std::ofstream myfile;
+            myfile.open ("experimento.csv");
+            myfile << "Parametros:\n";
+            myfile << "Dias maximos: "<< maximoDias << ", cantidad de ensayos de reproducción de los Evitas: " << M << ", cantidad de ensayos de reproducción de los Ángeles: " << N << ", probabilidad de nacimiento para un Evita: " << ps[0] << ", probabilidad de nacimiento para un Ángel: " << ps[1] << ", probabilidad de que una evita se vuelva berserk en un día: " << ps[2] << ", diferencia de poder para dar positivo" << eps << "\n";
+            myfile << "\n";
+            for(int i = 0; i < maximoDias; i++)
+            {
+                myfile << "Dia: " << i << ", Cantidad Evitas: " << evitas[i] << ", Cantidad Angeles: " << angeles[i] << "\n";
             }
+            myfile.close();
+            verificarResultado(eps, maximoDias, evitas, angeles);
+            exit(0);
         }
     }
-    for(int x = 0; x < n + m; x++)
-    {
-        wait(NULL);
-    }
+   
+    experimento(probabilidadNacimiento, probabilidadBerserker, intentosProcreacion, lista, maximoDias);
     exit(0);
 }
 
-float generarNumeroRandom(){
-    srand( time( NULL ) );
-    float num = ( "%1.3f\n", ( rand( ) % 1001 ) / 1000.0f );
-
-    return num;
-}
-
-
-
-tuple<string,double, int, int> crearHijos(float probabilidadNacimiento, float probabilidadBerserker, int intentosProcreacion)
+tuple<string,double, int, int, string> crearHijos(float probabilidadNacimiento, float probabilidadBerserker, int intentosProcreacion)
 {
-    //preguntar si proBerserker es 0 entonces es Angel crear variable tipo(string) y crear tupla para devolver tipo, pid
     string tipo;
-    tuple<string,double, int, int> result;
-    
+    tuple<string,double, int, int, string> result;
+    string naturaleza = "normal";
     if(probabilidadBerserker < 1.1)
     {
         tipo = "evita"; 
@@ -105,20 +119,28 @@ tuple<string,double, int, int> crearHijos(float probabilidadNacimiento, float pr
         float u = (tipo == "evita") ? 20 : 25;
 	    float std = (tipo == "evita") ? 5 : 10;
         unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-        default_random_engine generator (3);
+        default_random_engine generator (seed);
         uniform_real_distribution<double> distribution(0.0,1.0);
         normal_distribution<double> ap_distribution(u,std);
         if ( tipo == "evita" && generarNumeroRandom() <= probabilidadBerserker)
         {
-            result = make_tuple(tipo,ap_distribution(generator)*2, getpid(), getppid());
-            return result;
-            exit(0);
-        }
-        else{
             childPid = fork();
-            if (childPid == 0){
-                if(generarNumeroRandom() <= probabilidadNacimiento){
-                    result = make_tuple(tipo,ap_distribution(generator), getpid(), getppid());
+            if (childPid == 0)
+            {
+                naturaleza = "berserker";
+                result = make_tuple(tipo,ap_distribution(generator)*2, getpid(), getppid(), naturaleza);
+                return result;
+            }
+            
+        }
+        else
+        {
+            childPid = fork();
+            if (childPid == 0)
+            {
+                if(generarNumeroRandom() <= probabilidadNacimiento)
+                {
+                    result = make_tuple(tipo,fabsf(ap_distribution(generator)), getpid(), getppid(), naturaleza);
                     return result;
                 }
             }
@@ -133,4 +155,79 @@ tuple<string,double, int, int> crearHijos(float probabilidadNacimiento, float pr
         exit(0);
     }
     return result;
+}
+
+void* share_mem(int size)
+{
+	// Vamos a pedir size * sizeof(atomic<int>) para reservar suficiendte memoria 
+	// para un vector sizeof nos dice el tamaño del tipo atomic int.
+    void * mem;
+    if( MAP_FAILED == (mem = (atomic<float>*)mmap(NULL, sizeof(atomic<float>)*size, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0) ) )
+    {
+        perror( "mmap failed" );
+        exit( EXIT_FAILURE );
+    }
+    return mem;
+}
+
+float generarNumeroRandom()
+{
+    srand( time( NULL ) );
+    float num = ( "%1.3f\n", ( rand( ) % 1001 ) / 1000.0f );
+
+    return num;
+}
+
+
+void verificarResultado(float eps, int maximoDias, atomic<float>* evitas, atomic<float>* angeles)
+{
+    cout << "------------------RESULTADO FINAL ----------------" << endl;
+    for(int i = 1; i < maximoDias; i++)
+    {   
+        evitas[i] = evitas[i] + evitas[i - 1];
+        evitas[i + maximoDias] = evitas[i + maximoDias] + evitas[i - 1 + maximoDias];
+        angeles[i] = angeles[i] + angeles[i - 1];
+        angeles[i + maximoDias] = angeles[i + maximoDias] + angeles[i - 1 + maximoDias];
+        cout << "DIA: "<< i << endl;
+        cout << "CANTIDAD ANGELES NACIDOS: "<<angeles[i] - angeles[i - 1]<< endl;
+        cout << "PODER TOTAL ANGELES: "<<angeles[i + maximoDias] << endl;
+        cout << "CANTIDAD EVITAS NACIDOS: "<<evitas[i] - evitas[i - 1] << endl;
+        cout << "PODER TOTAL EVITAS: "<<evitas[i + maximoDias]  << endl;
+        if(fabsf(angeles[i + maximoDias] - evitas[i + maximoDias] ) < eps)
+        {
+            cout << "------------------SIMULACION EXITOSA ----------------" << endl;
+            cout << "En el dia: " << i << endl;
+            cout << "Hay: "<< angeles[i] << " angeles" << " , con un poder total de : "<< angeles[i + maximoDias] << endl;
+            cout << "Hay: "<< evitas[i] << " evitas" << " , con un poder total de : "<< evitas[i + maximoDias] << endl;
+            return;        
+        }
+    }                                                                                                               
+    cout << "No se encontro un dia de mismo poder" << endl;
+    return;
+}                                                                                       
+
+void agregarHijo(atomic<float>* lista, int dia, float poder, int maximoDias)
+{
+    lista[dia] =  lista[dia] + 1;
+    //cout << " poder antes de sumar: " << lista[dia + maximoDias] <<endl;
+    lista[dia + maximoDias] = lista[dia + maximoDias] + poder;
+    //cout << " poder despues de sumar: " << lista[dia + maximoDias] << endl;
+}
+
+void experimento(float probabilidadNacimiento, float probabilidadBerserker, int intentosProcreacion, atomic<float>* lista, int maximoDias)
+{
+    for (int i = 1; i < maximoDias; i++)
+    {    
+        auto datosHijo = crearHijos(probabilidadNacimiento,probabilidadBerserker,intentosProcreacion);
+        auto [tipo, poder, pid, ppid, naturaleza] = datosHijo;
+        //cout << tipo;
+        agregarHijo(lista, i , poder, maximoDias);
+        cout << "Tipo: " << tipo << " Poder: " << poder << " Pid: "<< pid <<" PPID:"<< ppid<< " DIA: "<< i <<endl;      
+        if(naturaleza == "berserker")
+        {
+            exit(0);
+        }
+        
+    }
+    return;
 }
